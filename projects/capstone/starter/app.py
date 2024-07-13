a223dd9bd8c19064fb20f4e3b029855b3a144f79
phuongghj123 <lodinhphuong791@gmail.com>
2024-07-13 10:26:50 +0700

    [Add] Add models and CORS for final project

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,13 +2,28 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import db, Movie, Actor
+from flask_moment import Moment
+from flask_migrate import Migrate
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  moment = Moment(app)
+  app.config.from_object('config')
+  db.init_app(app)
+  migrate = Migrate(app, db)
   CORS(app)
 
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+  
   return app
+
 
 APP = create_app()
 
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,27 @@
+from flask_sqlalchemy import *
+db = SQLAlchemy()
 
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime, nullable=False)
+    actors = db.relationship('Actor', backref='Movie')
+
+    def __repr__(self):
+        return f'<id: {self.id}, title: {self.title}, release_date: {self.release_date}>'
+
+
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey('Movie.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<id: {self.id}, name: {self.name}, age: {self.age}, gender: {self.gender}>'
